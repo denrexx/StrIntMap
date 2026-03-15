d292fb3e3b4254057e6e5e2d2c18fc299bc16f1b
denrexx <ffurry939@gmail.com>
2026-03-15 18:22:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,62 +6,44 @@ class StrIntMap:
 
   def get_index(self, key):
     return hash(key) % self.capacity
-  
-  def put(self, key, value):
-    index = self.get_index(key)
-    bucket = self.tab[index]
 
+  def put(self, key, value):
+    bucket = self.tab[self.get_index(key)]
     for p in bucket:
       if p[0] == key:
         p[1] = value
         return
-      
     bucket.append([key, value])
     self.cnt += 1
 
   def get(self, key):
-    index = self.get_index(key)
-    bucket = self.tab[index]
-
-    for p in bucket:
-      if p[0] == key:
-        return p[1]
+    for k,v in self.tab[self.get_index(key)]:
+      if k == key:
+        return v
     return None
-  
-  def remove(self, key):
-    index = self.get_index(key)
-    bucket = self.tab[index]
 
-    for i,p in enumerate(bucket):
-      if p[0] == key:
+  def remove(self, key):
+    bucket = self.tab[self.get_index(key)]
+    for i,(k,v) in enumerate(bucket):
+      if k == key:
         bucket.pop(i)
         self.cnt -= 1
         return
 
   def contains(self, key):
-    if self.get(key) is None:
-      return False
-    return True
-  
+    return self.get(key) is not None
+
   def size(self):
     return self.cnt
-  
-  def keys(self):
-    res = []
-    for bucket in self.tab:
-      for p in bucket:
-        res.append(p[0])
-    return res
-  
-  def rehash(self):
-    old_table = self.tab
 
-    self.capacity = self.capacity * 2
+  def keys(self):
+    return [k for bucket in self.tab for k,_ in bucket]
+
+  def rehash(self):
+    old = self.tab
+    self.capacity *= 2
     self.tab = [[] for _ in range(self.capacity)]
     self.cnt = 0
-
-    for bucket in old_table:
-      for pair in bucket:
-        key = pair[0]
-        value = pair[1]
-        self.put(key, value) 
+    for bucket in old:
+      for k,v in bucket:
+        self.put(k,v)
